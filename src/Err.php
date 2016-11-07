b9c6aafe548206115352d1b7c4d86bd27b499095
Proceed <steve@prcd.co>
2016-11-07 16:40:54 +0000

    Update Err.php
    Slight restructure and now if a log is required, all errors are logged (previously ignored errors were not logged).

--- src/Err.php
+++ src/Err.php
@@ -6,22 +6,32 @@
 class Err {
 
 	/**
-	 * @var array Of arrays holding error data to be logged
+	 * @var integer Number of background errors
 	 */
-	private static $error_list_log = [];
+	private static $count_error_background = 0;
 
 	/**
-	 * @var array Of arrays holding ignored error data
+	 * @var integer Number of ignored errors
 	 */
-	private static $error_list_ignore = [];
+	private static $count_error_ignore = 0;
 
 	/**
-	 * @var integer A bitwise derived integer made with PHP Error Constants controlling which error codes to log silently, default set in initialise
+	 * @var integer Number of termination errors
+	 */
+	private static $count_error_terminate = 0;
+
+	/**
+	 * @var array Holds all error data
+	 */
+	private static $errors = [];
+
+	/**
+	 * @var integer A bitwise derived integer made with PHP Error Constants controlling which error codes to log silently
 	 */
 	private static $errors_background = null;
 
 	/**
-	 * @var integer A bitwise derived integer made with PHP Error Constants controlling which error codes to ignore, default set in initialise
+	 * @var integer A bitwise derived integer made with PHP Error Constants controlling which error codes to ignore
 	 */
 	private static $errors_ignore = null;
 
@@ -41,38 +51,122 @@ class Err {
 	private static $log_file_terminate = 'terminate.txt';
 
 	/**
-	 * @var bool Whether or not the script terminated due do an error
-	 */
-	private static $script_terminated = false;
-
-	/**
 	 * @var bool|string A string to echo when the script is terminated, otherwise the error logged is dumped
 	 */
 	private static $termination_message = false;
 
 	/**
-	 * @var string The timestamp to record with logged errors, default set in initialise
+	 * @var string Timestamp to use in log file with logged errors
 	 */
 	private static $timestamp = null;
 
+	
+	public static function errorHandler($err_no, $err_str, $err_file, $err_line)
+	{
+		if (self::$errors_ignore & $err_no) {
+			self::$count_error_ignore++;
+			self::$errors[] = [
+				'error'     => $err_no,
+				'message'   => $err_str,
+				'file'      => $err_file,
+				'line'      => $err_line,
+				'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
+			];
+		} else {
+			self::$count_error_background++;
+			self::$errors[] = [
+				'error'     => $err_no,
+				'message'   => $err_str,
+				'file'      => $err_file,
+				'line'      => $err_line,
+				'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
+			];
+			if (!(self::$errors_background & $err_no)) {
+				// ...the script should be terminated
+				self::$count_error_terminate++;
+				self::shutdownFinal();
+			}
+		}
+	}
 
 	/**
-	 * Extract any logged error data so far and clear the data due to be logged
+	 * Extract any error data so far then reset
+	 * @param bool $with_counts Return error counts as well?
 	 * @return array
 	 */
-	public static function extract()
+	public static function extract($with_counts = false)
 	{
-		$data = [
-			'background' => self::$error_list_log,
-			'ignore' => self::$error_list_ignore
-		];
+		if ($with_counts) {
+			$data = [
+				'counts' => [
+					'ignore' => self::$count_error_ignore,
+					'background' => self::$count_error_background,
+					'terminate' => self::$count_error_terminate
+				],
+				'errors' => self::$errors
+			];
+		} else {
+			$data = self::$errors;
+		}
 
-		self::$error_list_log = [];
-		self::$error_list_ignore = [];
+		self::$errors = [];
+		self::$count_error_background = 0;
+		self::$count_error_ignore = 0;
+		self::$count_error_terminate = 0;
 
 		return $data;
 	}
 
+	/**
+	 * Get an error name from its integer value
+	 * @param $error_code int
+	 * @return string The name of the error code submitted
+	 */
+	public static function getName($error_code)
+	{
+		switch ($error_code) {
+			case E_ERROR: // 1
+				return 'E_ERROR';
+			case E_WARNING: // 2
+				return 'E_WARNING';
+			case E_PARSE: // 4
+				return 'E_PARSE';
+			case E_NOTICE: // 8
+				return 'E_NOTICE';
+			case E_CORE_ERROR: // 16
+				return 'E_CORE_ERROR';
+			case E_CORE_WARNING: // 32
+				return 'E_CORE_WARNING';
+			case E_COMPILE_ERROR: // 64
+				return 'E_COMPILE_ERROR';
+			case E_COMPILE_WARNING: // 128
+				return 'E_COMPILE_WARNING';
+			case E_USER_ERROR: // 256
+				return 'E_USER_ERROR';
+			case E_USER_WARNING: // 512
+				return 'E_USER_WARNING';
+			case E_USER_NOTICE: // 1024
+				return 'E_USER_NOTICE';
+			case E_STRICT: // 2048
+				return 'E_STRICT';
+			case E_RECOVERABLE_ERROR: // 4096
+				return 'E_RECOVERABLE_ERROR';
+			case E_DEPRECATED: // 8192
+				return 'E_DEPRECATED';
+			case E_USER_DEPRECATED: // 16384
+				return 'E_USER_DEPRECATED';
+			case E_ALL: // 32767
+				return 'E_ALL';
+			default:
+				return 'UNKNOWN_ERROR_CODE';
+		}
+	}
+
+	/**
+	 * Initialise error logging
+	 * @param null|array $parameters Array of parameters as expected by setParametersWithArray()
+	 * @throws Exception if log files do not exist or cannot be written to
+	 */
 	public static function initialise($parameters = null)
 	{
 		if (is_array($parameters)) {
@@ -98,38 +192,10 @@ class Err {
 		// do not display errors
 		error_reporting(0);
 
-		// register functions
-		set_error_handler('Err::handler');
+		// register error handling functions
+		set_error_handler('Err::errorHandler');
 		register_shutdown_function('Err::shutdownCheckForFatal');
 		register_shutdown_function('Err::shutdownFinal');
-	}
-
-	public static function handler($err_no, $err_str, $err_file, $err_line)
-	{
-		if (self::$errors_ignore & $err_no) {
-			// this error should not be logged, the script can continue
-			self::$error_list_ignore[] = [
-				'error'     => $err_no,
-				'message'   => $err_str,
-				'file'      => $err_file,
-				'line'      => $err_line,
-				'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
-			];
-		} else {
-			// this error should be logged...
-			self::$error_list_log[] = [
-				'error'     => $err_no,
-				'message'   => $err_str,
-				'file'      => $err_file,
-				'line'      => $err_line,
-				'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
-			];
-			if (!(self::$errors_background & $err_no)) {
-				// ...the script should be terminated
-				self::$script_terminated = true;
-				self::shutdownFinal();
-			}
-		}
 	}
 
 	public static function setErrorsBackground($errors)
@@ -163,7 +229,7 @@ class Err {
 	 */
 	public static function setParametersWithArray($parameters)
 	{
-		$can_set = array_flip([
+		$can_set = [
 			'errors_to_ignore',
 			'errors_to_log',
 			'log_directory',
@@ -171,10 +237,10 @@ class Err {
 			'log_file_terminate',
 			'termination_message',
 			'timestamp'
-		]);
+		];
 
 		foreach ($parameters as $name => $value) {
-			if (array_key_exists($name, $can_set)) {
+			if (in_array($name, $can_set)) {
 				self::${$name} = $value;
 			}
 		}
@@ -186,26 +252,26 @@ class Err {
 	 */
 	public static function shutdownCheckForFatal()
 	{
-		// get the last error
-		$error = error_get_last();
-
 		// the following are fatal errors which will not be processed by the function set in set_error_handler()...
 		$core_fatal = [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR];
 
+		// get the last error
+		$error = error_get_last();
+
 		// ...so if the last error matches, record that the script has terminated and pass details to be processed
 		if ($error !== NULL && in_array($error['type'], $core_fatal, true)) {
-			self::$script_terminated = true;
-			self::handler($error['type'], $error['message'], $error['file'], $error['line']);
+			self::$count_error_terminate++;
+			self::errorHandler($error['type'], $error['message'], $error['file'], $error['line']);
 		}
 	}
 
 	/**
-	 * Registered as shutdown function. Performs final clean up, logging errors
-	 * if needed and outputting any required data.
+	 * Registered as shutdown function. Performs final clean up, logging
+	 * errors if needed and outputting any required data.
 	 */
 	public static function shutdownFinal()
 	{
-		if (self::$script_terminated && self::$termination_message === false) {
+		if (self::$count_error_terminate > 0 && self::$termination_message === false) {
 			echo '<hr>';
 			echo '<h1>Script Terminated by PHP Error</h1>';
 			echo '<hr>';
@@ -214,9 +280,15 @@ class Err {
 			exit;
 		}
 
-		if (self::$error_list_log) {
-			$log_file_path = self::$log_directory . '/' . (self::$script_terminated ? self::$log_file_terminate : self::$log_file_background);
+		if (self::$count_error_terminate > 0) {
+			$log_file_path = self::$log_directory . '/' . self::$log_file_terminate;
+		} else if (self::$count_error_background > 0) {
+			$log_file_path = self::$log_directory . '/' . self::$log_file_background;
+		} else {
+			$log_file_path = null;
+		}
 
+		if ($log_file_path) {
 			$data = [
 				'timestamp' => self::$timestamp,
 				'log' => self::extract()
@@ -225,7 +297,7 @@ class Err {
 			file_put_contents($log_file_path, json_encode($data) . "\n", FILE_APPEND | LOCK_EX);
 		}
 
-		if (self::$script_terminated) {
+		if (self::$count_error_terminate > 0) {
 			echo self::$termination_message;
 			exit;
 		}

da9f43ab7995503fc45a34281151fc42be7f8e16
Proceed <steve@prcd.co>
2016-11-11 17:41:39 +0000

    Update Err.php
    Tidied up code, added some DocBlocks. Added class addLogData() enabling extra data to be stored with log.

--- src/Err.php
+++ src/Err.php
@@ -8,17 +8,17 @@ class Err {
 	/**
 	 * @var integer Number of background errors
 	 */
-	private static $count_error_background = 0;
+	private static $error_count_background = 0;
 
 	/**
 	 * @var integer Number of ignored errors
 	 */
-	private static $count_error_ignore = 0;
+	private static $error_count_ignore = 0;
 
 	/**
-	 * @var integer Number of termination errors
+	 * @var integer Number of terminal errors
 	 */
-	private static $count_error_terminate = 0;
+	private static $error_count_terminal = 0;
 
 	/**
 	 * @var array Holds all error data
@@ -36,6 +36,11 @@ class Err {
 	private static $errors_ignore = 0;
 
 	/**
+	 * @var array Extra data to save with log
+	 */
+	private static $extra_log_data = [];
+
+	/**
 	 * @var string The path to log directory
 	 */
 	private static $log_directory = '';
@@ -46,51 +51,67 @@ class Err {
 	private static $log_file_background = 'background.txt';
 
 	/**
-	 * @var string The name of the file where terminating errors will be logged
+	 * @var string The name of the file where terminal errors will be logged
 	 */
-	private static $log_file_terminate = 'terminate.txt';
+	private static $log_file_terminal = 'terminal.txt';
 
 	/**
-	 * @var bool If shutdown procedure has run or not
+	 * @var bool If shutdown (log/output errors) has happened
 	 */
 	private static $shutdown_complete = false;
 
 	/**
 	 * @var false|string A string to echo when the script is terminated, otherwise the error log is dumped
 	 */
-	private static $termination_message = false;
+	private static $terminal_message = false;
 
 	/**
 	 * @var string Timestamp to use in log file with logged errors
 	 */
 	private static $timestamp = '';
 
+	/**
+	 * Add extra details to save with error log. Log array will include an extra
+	 * element called "data" which will contain the submitted details. Array must
+	 * be 1 dimensional, values are typecast as strings.
+	 * @param $data_array array Data to add to log
+	 */
+	public static function addLogData($data_array)
+	{
+		if (!is_array($data_array)) {
+			throw new Exception('Input must be an array');
+		}
 
+		foreach ($data_array as $key => $value) {
+			self::$extra_log_data[$key] = (string) $value;
+		}
+	}
+
+	/**
+	 * Custom error handler registered with set_error_handler()
+	 * @param $err_no
+	 * @param $err_str
+	 * @param $err_file
+	 * @param $err_line
+	 */
 	public static function errorHandler($err_no, $err_str, $err_file, $err_line)
 	{
+		// store error details
+		self::$errors[] = [
+			'error'     => $err_no,
+			'message'   => $err_str,
+			'file'      => $err_file,
+			'line'      => $err_line,
+			'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
+		];
+
 		if (self::$errors_ignore & $err_no) {
-			self::$count_error_ignore++;
-			self::$errors[] = [
-				'error'     => $err_no,
-				'message'   => $err_str,
-				'file'      => $err_file,
-				'line'      => $err_line,
-				'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
-			];
+			self::$error_count_ignore++;
+		} else if (self::$errors_background & $err_no) {
+			self::$error_count_background++;
 		} else {
-			self::$count_error_background++;
-			self::$errors[] = [
-				'error'     => $err_no,
-				'message'   => $err_str,
-				'file'      => $err_file,
-				'line'      => $err_line,
-				'backtrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
-			];
-			if (!(self::$errors_background & $err_no)) {
-				// ...the script should be terminated
-				self::$count_error_terminate++;
-				self::shutdownFinal();
-			}
+			self::$error_count_terminal++;
+			self::shutdownFinal();
 		}
 	}
 
@@ -104,21 +125,19 @@ class Err {
 		if ($with_counts) {
 			$data = [
 				'counts' => [
-					'ignore' => self::$count_error_ignore,
-					'background' => self::$count_error_background,
-					'terminate' => self::$count_error_terminate
+					'ignore' => self::$error_count_ignore,
+					'background' => self::$error_count_background,
+					'terminal' => self::$error_count_terminal
 				],
 				'errors' => self::$errors
 			];
 		} else {
 			$data = self::$errors;
 		}
-
 		self::$errors = [];
-		self::$count_error_background = 0;
-		self::$count_error_ignore = 0;
-		self::$count_error_terminate = 0;
-
+		self::$error_count_background = 0;
+		self::$error_count_ignore = 0;
+		self::$error_count_terminal = 0;
 		return $data;
 	}
 
@@ -179,7 +198,7 @@ class Err {
 		}
 
 		// check for write permissions to log files
-		if (!is_writable(self::$log_directory . '/' . self::$log_file_background) || !is_writable(self::$log_directory . '/' . self::$log_file_terminate)) {
+		if (!is_writable(self::$log_directory . '/' . self::$log_file_background) || !is_writable(self::$log_directory . '/' . self::$log_file_terminal)) {
 			throw new Exception('Err class cannot write to log files or log files do not exist.');
 		}
 
@@ -187,9 +206,11 @@ class Err {
 		if (self::$timestamp === '') {
 			self::$timestamp = time();
 		}
+
 		if (self::$errors_ignore === 0) {
 			self::$errors_ignore = E_NOTICE | E_USER_NOTICE;
 		}
+
 		if (self::$errors_background === 0) {
 			self::$errors_background = E_WARNING | E_CORE_WARNING | E_COMPILE_WARNING | E_USER_WARNING | E_DEPRECATED | E_USER_DEPRECATED;
 		}
@@ -223,14 +244,14 @@ class Err {
 		self::$log_file_background = $file_name;
 	}
 
-	public static function setLogFileTerminate($file_name)
+	public static function setLogFileTerminal($file_name)
 	{
-		self::$log_file_terminate = $file_name;
+		self::$log_file_terminal = $file_name;
 	}
 
-	public static function setTerminationMessage($termination_message)
+	public static function setTerminalMessage($terminal_message)
 	{
-		self::$termination_message = $termination_message;
+		self::$terminal_message = $terminal_message;
 	}
 
 	/**
@@ -244,11 +265,10 @@ class Err {
 			'errors_ignore',
 			'log_directory',
 			'log_file_background',
-			'log_file_terminate',
-			'termination_message',
+			'log_file_terminal',
+			'terminal_message',
 			'timestamp'
 		];
-
 		foreach ($parameters as $name => $value) {
 			if (in_array($name, $can_set)) {
 				self::${$name} = $value;
@@ -271,45 +291,50 @@ class Err {
 
 		// If the last error has a match in $core_fatal, record that the script has terminated and pass details to be processed
 		if ($error !== NULL && in_array($error['type'], $core_fatal, true)) {
-			self::$count_error_terminate++;
+			self::$error_count_terminal++;
 			self::errorHandler($error['type'], $error['message'], $error['file'], $error['line']);
 		}
 	}
 
 	/**
-	 * Registered as shutdown function. Performs final clean up, logging
-	 * errors if needed and outputting any required data.
+	 * Registered as shutdown function and called if a terminal error occurs.
+	 * Ensures that shutdownProcedure() (log or dump) is performed once.
 	 */
 	public static function shutdownFinal()
 	{
-		if (self::$shutdown_complete) {
-			return;
+		if (self::$shutdown_complete === false) {
+			self::$shutdown_complete = true;
+			self::shutdownProcedure();
 		}
+	}
 
-		self::$shutdown_complete = true;
-		
-		if (self::$count_error_terminate > 0 && self::$termination_message === false) {
+	/**
+	 * Perform final procedure. Actions depends on the type of errors occurred
+	 * and terminal message value.
+	 */
+	private static function shutdownProcedure()
+	{
+		if (self::$error_count_terminal > 0 && self::$terminal_message === false) {
 			echo '<hr>';
 			echo '<h1>Script Terminated by PHP Error</h1>';
 			echo '<hr>';
 			echo '<pre>';
-			print_r(self::extract());
+			print_r(self::$errors);
 			exit;
 		}
 
-		if (self::$count_error_terminate > 0 || self::$count_error_background > 0) {
-			$log_file = self::$log_directory . '/' . (self::$count_error_terminate > 0 ? self::$log_file_terminate : self::$log_file_background);
-
-			$data = [
-				'timestamp' => self::$timestamp,
-				'log' => self::$errors
-			];
-
+		if (self::$error_count_terminal > 0 || self::$error_count_background > 0) {
+			$log_file = self::$log_directory . '/' . (self::$error_count_terminal > 0 ? self::$log_file_terminal : self::$log_file_background);
+			$data['timestamp'] = self::$timestamp;
+			if (self::$extra_log_data) {
+				$data['data'] = self::$extra_log_data;
+			}
+			$data['log'] = self::$errors;
 			file_put_contents($log_file, json_encode($data) . "\n", FILE_APPEND | LOCK_EX);
 		}
 
-		if (self::$count_error_terminate > 0) {
-			echo self::$termination_message;
+		if (self::$error_count_terminal > 0) {
+			echo self::$terminal_message;
 			exit;
 		}
 	}
